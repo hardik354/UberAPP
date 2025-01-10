02b5a45b51f3f1b9fe2d31d22141743834e64702
hardik354 <hardikjain.it25@gmail.com>
2025-01-10 20:02:05 +0530

    user authentication done

--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -2,7 +2,10 @@ const express = require("express");
 const router = express.Router();
 const { body } = require("express-validator");
 const userController = require("../controllers/user.controller");
+const authMiddleware = require('../middlewares/auth.middleware');
 
+
+// register routes
 router.post(
   "/register",
   [
@@ -17,12 +20,20 @@ router.post(
   userController.registerUser
 );
 
+// login routes
 router.post('/login', [
     body('email').isEmail().withMessage('Invalid Email'),
     body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long')
 ],
     userController.loginUser
 )
+
+// profile routes
+router.get('/profile', authMiddleware.authUser, userController.getUserProfile)
+
+// logout routes
+router.get('/logout', authMiddleware.authUser, userController.logoutUser)
+
 
 
 module.exports = router;
